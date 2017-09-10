8f6a165ed550c3695621793f6c386bf93b934c85
ryu2020 <ryu2020@pingry.org>
2017-09-09 23:16:04 -0400

    Update url.js

--- url.js
+++ url.js
@@ -2,6 +2,9 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+
+const {Builder, By, until} = require('selenium-webdriver');
+
 var app     = express();
 
 function hc(str){
@@ -10,10 +13,31 @@ function hc(str){
 
 
 
-    return "https://healthcarebluebook.com/page_ProcedureDetails" + link.subString(11)
+    return "https://healthcarebluebook.com/page_ProcedureDetails" + link.substring(11)
 }
 
 function chc(str){
-    var CH = "https://clearhealthcosts.com/search/?query=" + str + "+X-ray+exam+upper+gi+tract&zip_code=19019&radius=25&no_zero=1&submit="
+    var CH = "https://clearhealthcosts.com/search/?query=" + str + "+X-ray+exam+upper+gi+tract&zip_code=19019&radius=2500&no_zero=1&submit="
     return CH;
+}
+
+function fh(str){
+  var driver = new Builder()
+  .forBrowser('chrome')
+  .build();
+
+driver.get('https://www.fairhealthconsumer.org/estimate-costs/step-1');
+radio = driver.findElement(By.className('indicator ion-ios-circle-outline'))
+radio.click();
+driver.findElement(By.className('step1Btn')).click();
+driver.findElement(By.className('form-control ui-autocomplete-input')).sendKeys("19104");
+driver.findElement(By.className('step2Btn')).click();
+//setTimeout(function(){driver.findElement(By.className('form-control ui-autocomplete-input')).sendKeys("aaaaa");}, 1000);
+
+
+
+
+
+driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+driver.quit();
 }
