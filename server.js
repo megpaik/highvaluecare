1c598cdca2dab784a0ed5410706f288a50b284dd
oliverchanyw <oliverchanyw@gmail.com>
2017-09-09 21:30:52 -0400

    First working version of server.js

--- server.js
+++ server.js
@@ -7,49 +7,75 @@ var app     = express();
 var buffer2write = {};
 
 // what to do when we visit '/scrape'
-app.get('/scrape', function(req, res) {
+app.get('/scrape', (req, res) => {
 
-  var testURL1 = 'https://healthcarebluebook.com/page_ProcedureDetails.aspx?cftId=390&g=Abdomen+and+Pelvis+CT+(no+contrast)';
-  var testURL2 = 'https://clearhealthcosts.com/blog/procedure/ct-scan-pelvis-without-contrast/';
-  var testURL3 = 'https://www.newchoicehealth.com/places/pennsylvania/philadelphia/ct-scan/ct-angiography-abdomen';
+  // first, request, then if goes well, scrape with it
+  var promise1 = test1()
+  .then((fulfilVal) => { scrapePrice(fulfilVal.html, fulfilVal.hostName) })
+  .catch(error => console.log(error));
 
-  // test 1
-  request(testURL1, function(error, response, html) {
-    if (error) { console.log("error"); }
-    scrapePrice(html, 'HCBB')
+  var promise2 = test2()
+  .then((fulfilVal) => { scrapePrice(fulfilVal.html, fulfilVal.hostName) })
+  .catch(error => console.log(error));
 
-    // write the File, replace later
+  var promise3 = test3()
+  .then((fulfilVal) => { scrapePrice(fulfilVal.html, fulfilVal.hostName) })
+  .catch(error => console.log(error));
+
+  Promise.all([promise1, promise2, promise3])
+  .then(() => {
+    // finish by writing the file
     fs.writeFile('output.json', JSON.stringify(buffer2write, null, 4), function(err){
       console.log('File  written!');
     })
-    // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
-    res.send('Check your console!')
-  });
 
-  // test 2
-  request(testURL2, function(error, response, html) {
-    if (error) { console.log("error"); }
-    scrapePrice(html, 'CH')
-    // write the File, replace later
-    fs.writeFile('output.json', JSON.stringify(buffer2write, null, 4), function(err){
-      console.log('File  written!');
-    })
-    // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
+    // finally, we'll just send out a message to the browser
     res.send('Check your console!')
-  });
-
-  // test 3
-  request(testURL3, function(error, response, html) {
-    if (error) { console.log("error"); }
-    scrapePrice(html, 'NCH')
-    // write the File, replace later
-    fs.writeFile('output.json', JSON.stringify(buffer2write, null, 4), function(err){
-      console.log('File  written!');
-    })
-    // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
-    res.send('Check your console!')
-  });
+  })
 });
+
+// run our tests: resolve = func(html, hostName); reject = func(error)
+// test 1
+function test1() {
+  return new Promise( function(resolve, reject) {
+    var testURL1 = 'https://healthcarebluebook.com/page_ProcedureDetails.aspx?cftId=390&g=Abdomen+and+Pelvis+CT+(no+contrast)';
+    request(testURL1, (error, response, html) => {
+      if (error) reject(error);
+      else {
+        var fulfilVal = {html: html, hostName : 'HCBB'}
+        resolve(fulfilVal);
+      }
+    })
+  })
+}
+
+// test 2
+function test2() {
+  return new Promise(function(resolve, reject) {
+    var testURL2 = 'https://clearhealthcosts.com/blog/procedure/ct-scan-pelvis-without-contrast/';
+    request(testURL2, (error, response, html) => {
+      if (error) reject(error);
+      else {
+        var fulfilVal = {html: html, hostName : 'CH'}
+        resolve(fulfilVal);
+      }
+    });
+  })
+}
+
+// test 3
+function test3() {
+  return new Promise(function(resolve, reject) {
+    var testURL3 = 'https://www.newchoicehealth.com/places/pennsylvania/philadelphia/ct-scan/ct-angiography-abdomen';
+    request(testURL3, (error, response, html) => {
+      if (error) reject(error);
+      else {
+        var fulfilVal = {html: html, hostName : 'NCH'}
+        resolve(fulfilVal);
+      }
+    });
+  })
+}
 
 // scrape a website (generic)
 var scrapePrice = function(html, hostName) {
@@ -70,24 +96,22 @@ var scrapePrice = function(html, hostName) {
 }
 
 // HealthCareBlueBook helper (specific to their HTML)
-var HCBBreq = function($) {
-  console.log("We got to HCBBreq");
+HCBBreq = function($) {
   var price = -1;
   // get els with matching tag, and run a function on them?
   $('.arrow-box').each(function(i, elem){
     var text = $(this).children().first().text();
-    price = parseInt(text.replace('$', '').replace(',',''));
+    price = parseInt(text.replace(/\D/g,''));
   })
   buffer2write.HCBBprice = price;
 }
 
 // ClearHealth helper (specific to their HTML)
 var CHreq = function($) {
-  console.log("We got to CHreq");
   var prices = [];
-  $('.price-badge price-charged').each(function(i, elem){
-    var text = $(this).children().first().text();
-    prices.push(parseInt(text.replace('$', '').replace(',','')));
+  $('.price-badge.price-charged').each(function(i, elem){
+    var text = $(this).children().last().text();
+    prices.push(parseInt(text.replace(/\D/g,'')));
   })
   prices.sort();
   buffer2write.CHprice = prices[Math.floor(prices.length / 2)];
@@ -95,16 +119,15 @@ var CHreq = function($) {
 
 // FairHealth helper (specific to their HTML)
 var FHreq = function($) {
-  console.log("We got to FHreq");
   var priceInsured = -1;
   var priceUninsured = -1;
   $('.circle out-net-summary').each(function(i, elem){
     var textUninsured = $(this).text();
-    priceUninsured = parseInt(text.replace('$', '').replace(',',''));
+    priceUninsured = parseInt(textUninsured.replace(/\D/g,''));
   })
   $('.circle in-net-summary small').forEach(function(){
-    var data = textInsured = $(this).text();
-    priceInsured = parseInt(text.replace('$', '').replace(',',''));
+    var textInsured = $(this).text();
+    priceInsured = parseInt(textInsured.replace(/\D/g,''));
   })
 
   // TO-DO: make a decision about price somehow
@@ -113,15 +136,14 @@ var FHreq = function($) {
 
 // NewChoiceHealth helper (specific to their HTML)
 var NCHreq = function($) {
-  console.log("We got to NCHreq");
-  var price = 0;
-  $('.pull-left cost average').each(function(i, elem){
+  var price = -1;
+  $('.pull-left.cost.average').each(function(i, elem){
     var text = $(this).text();
-    text = parseInt(text.replace('$', '').replace(',','').replace(' ',''));
+    console.log(text);
+    price = parseInt(text.replace(/\D/g,''));
   })
   buffer2write.NCHprice = price;
 }
-
 
 app.listen('8081');
 console.log('The magic happens on port 8081');
