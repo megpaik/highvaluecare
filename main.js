c4bd286ecd1312edc51317e79ee7f288b29c3545
Megan Paik <paik.megan@gmail.com>
2017-09-20 16:44:04 -0400

    update to main

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-document.title = "Penn Medicine Searcher";
+document.title = "highvalue_care";
 
 // Store data from static CSV file
 var macro = [];
 // make lower case data available for search
 var sorted = [];
+// this is the capitalzied sorted array
 var result = [];
 // list of current items that are have already processed
 var currItems = [];
@@ -14,12 +15,10 @@ Papa.parse("https://raw.githubusercontent.com/megpaik/pennapps/master/studies.cs
     download: true,
     skipEmptyLines: true,    
 	complete: function(results) {
-        // this is raw data
         macro = results.data;
-        // now sort the array
         sorted = sort(macro);
-        // now capitalize the array
         result = capitalize(sorted);
+        console.log('parse studies');
 	}
 });
 
@@ -27,10 +26,8 @@ Papa.parse("https://raw.githubusercontent.com/megpaik/pennapps/master/lab-fees.c
     download: true,
     skipEmptyLines: true,    
 	complete: function(results) {
-        // this is raw data
         var rawPrices = results.data;
-        console.log(result[0][7]);
-        console.log(rawPrices[0][3]);
+        // This populates the cost column.
         for (var i = 0; i < rawPrices.length; i++) {
             for (var j = 0; j < result.length; j++) {
                 if (result[j][1] === rawPrices[i][0]) {
@@ -40,14 +37,17 @@ Papa.parse("https://raw.githubusercontent.com/megpaik/pennapps/master/lab-fees.c
                 }
             }
         }
+        console.log('parse cost pdf')
         listfill();
     }
 });
 
 var listfill = function() {
-        for (var i = 1; i < result.length; i++) {
-            addStudy('#toplist', result[i]);                
-        }
+    for (var i = 1; i < result.length; i++) {
+        addStudy('#toplist', result[i]);                
+    }
+    console.log('listfill');
+    currItems = [];
 }
 
 // sorting algorithm for 2D array based on the first column
@@ -58,6 +58,7 @@ var sort = function(arr) {
             if (j < 1) { arr[i][j] = arr[i][j].toLowerCase(); }
         }
     }
+    console.log('sort')
     return arr.sort();
 }
 
@@ -69,6 +70,7 @@ var capitalize = function (array) {
             else { capitalized[i][j] = array[i][j]; }
         }
     }
+    console.log('capitalize')
     return capitalized;
 }
 
@@ -78,6 +80,7 @@ var init = function(num1, num2) {
     for (var i = 0; i < num1; i++) {
       temp[i] = new Array(num2);
     }
+    console.log('init array')
     return temp;
 }
 
@@ -85,26 +88,10 @@ var init = function(num1, num2) {
 var changePage = function() {
     $('#directory').toggleClass("active");
     $('#container').toggleClass("active");
-    currItmes = [];
     console.log("changePage called");
 }
 
-// this changes between search results and main page
-var searchPage = function() {
-    $('#results').toggleClass("active");
-    $('#container').toggleClass("active");
-    $
-    var bool = $('#results').hasClass("active");
-    if (!bool) {
-        $('#resultlist').empty();
-        document.querySelector("input[type=text]").value = null;
-        currItems = [];
-    }
-    console.log("searchPage called");
-}
-
 var createTable = function (arr) {
-    // Create table.
     var table = document.createElement('table');
     var row1 = table.insertRow(0);
     var row2 = table.insertRow(1);
@@ -119,27 +106,29 @@ var createTable = function (arr) {
 var addStudy = function(node, arr) {
     if (!currItems.includes(arr[0])) {
         $(node).append("<div class='item' id='" + arr[0] + "'></div>");
-        var parent = document.getElementById(arr[0]);
+        var parent = document.getElementById(arr[0]);                    
         var text = document.createTextNode(arr[0]);
         parent.appendChild(text);
         currItems.push(arr[0]);
     }    
-    var parent = document.getElementById(arr[0]); 
     // create a UNIQUELY TAGGED child node that stores the information
+    var parent = document.getElementById(arr[0]);        
     var tag = arr[0]+arr[1]+arr[2]+arr[3];
     $(parent).append("<div class='description' id='" + tag + "'></div>");    
     var miniBox = document.getElementById(tag);
     var description = createTable(arr);
     miniBox.appendChild(description);
+    //handle comments
     if (arr[8] !== "") {
         $(miniBox).append("<div class='comments'> *  " + arr[8] + "</div>");
     }
+    // ~sliding~ toggle your welc
     $(parent).click(function() {
         $(miniBox).slideToggle("fast", function() {});
     });
 }
 
-
+// TESTING SERVER INTERACTION
 // dummy.addEventListener("click", function() {
 //         var xhr = new XMLHttpRequest();
 //         xhr.open( "GET", '/data', true);
@@ -149,14 +138,29 @@ var addStudy = function(node, arr) {
 //        console.log(res.value);
 //     });
 
+// this changes between search results and main page
+var searchPage = function() {
+    $('#results').toggleClass("active");
+    $('#container').toggleClass("active");
+    var bool = $('#results').hasClass("active");
+    if (!bool) {
+        $('#resultlist').empty();
+        document.querySelector("input[type=text]").value = null;
+    }
+    console.log("searchPage called");
+}
 
+// handles search 
 var searchEvent = function(keyword) {
-    for (var i = 0; i < sorted.length; i++) {
+            console.log(currItems);
+
+    for (var i = 0; i < result.length; i++) {
         $('#resulthead').text('Results for: "' + keyword + '"');        
         var target = sorted[i][0];
-        var targetDiag = sorted[i][2];
-        if (target.indexOf(keyword) >= 0 || targetDiag.indexOf(keyword) >= 0) {
+        var diag = sorted[i][2];
+        if (target.indexOf(keyword) >= 0 || diag.indexOf(keyword) >= 0) {
             addStudy('#resultlist', sorted[i]);
+            console.log(currItems);
         }
     }
     searchPage();
@@ -166,6 +170,7 @@ var searchEvent = function(keyword) {
 $('#submit').click(function() {
     var task = document.querySelector("input[type=text]")
     var input = task.value.toLowerCase();
+    currItems = [];
     searchEvent(input);
 });
 
