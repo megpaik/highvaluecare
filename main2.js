61d5dc0a570dc3cd0a89f9354450d7a534eb32d3
Megan Paik <paik.megan@gmail.com>
2017-10-01 16:19:24 -0400

    search page updated

--- main2.js
+++ main2.js
@@ -1,21 +1,10 @@
-document.title = "highvalue_care";
-
-// // Store data from static CSV file
-// var macro = [];
-// // make lower case data available for search
-// var sorted = [];
-// // this is the capitalzied sorted array
-// var result = [];
-// // list of current items that are have already processed
-// var currItems = [];
+document.title = "highvalue.care";
 
 var studies = {};
-var currItems = {};
-
 var tableheadings = ['CPT', 'Intended Diagnosis', 'Sensitivity', 'Specificity', 'PPV', 'NPV', 'Cost'];
 
 // Read from JSON file (on server) and take those results
-var readStudiesReq = $.getJSON( "testStudies.json", (json) => {
+var readStudiesReq = $.getJSON( "https://raw.githubusercontent.com/megpaik/pennapps/master/testStudies.json", (json) => {
   studies = alphabetize(clean(json));
   listfill(studies);
 });
@@ -63,37 +52,36 @@ var alphabetize = function(studies, expected) {
 
 // Helper 3 - Fills the page with all studies
 var listfill = function(studies) {
+  var currItems = {};
   for (var study in studies) {
-    if (studies.hasOwnProperty(study)) { addStudy('#toplist', studies[study]); }
+    if (studies.hasOwnProperty(study)) { addStudy('toplist', studies[study], currItems); }
   }
 }
 
 // Helper 3a - adds a single study to the page by taking its object
-var addStudy = function(node, study) {
+var addStudy = function(node, study, items) {
   // Check currItems for existing study header, if doesn't exist, make one
-  if (!currItems.hasOwnProperty(study.name)) {
-    $(node).append("<div class='item' id='" + study.name + "'></div>");
-    var parent = document.getElementById(study.name);
-    parent.appendChild(document.createTextNode(study.name));
-    currItems[study.name] = {taken: true};
+  if (!items.hasOwnProperty(study.name)) {
+    $('#' + node).append("<div class='item' id='" + node + study.name + "'>" + study.name + "</div>");
+    items[study.name] = {taken: true};
   }
 
   // Now we're inside the study header, add the object with a unique tag
-  var parent = document.getElementById(study.name);
-  $(parent).append("<div class='description' id='" + study.ID + "'></div>");
-  var miniBox = document.getElementById(study.ID);
+  var inner = document.getElementById(node + study.name);
+  $(inner).append("<div class='description' id='" + node + study.ID + "'></div>");
+  var miniBox = document.getElementById(node + study.ID);
   var description = createTable(study);
   miniBox.appendChild(description);
 
   //handle comments
   if (study.hasOwnProperty('comments')) {
     for (c in study.comments) {
-      $(miniBox).append("<div class='comments'> *  " + study[c] + "</div>");
+      $(miniBox).append("<div class='comments'> *  " + c + "</div>");
     }
   }
 
   // ~sliding~ toggle
-  $(parent).click(function() {
+  $(inner).click(function() {
       $(miniBox).slideToggle("fast", function() {});
   });
 }
@@ -119,16 +107,6 @@ var changePage = function() {
     console.log("changePage called");
 }
 
-// TESTING COST SERVER INTERACTION
-// dummy.addEventListener("click", function() {
-//         var xhr = new XMLHttpRequest();
-//         xhr.open( "GET", '/data', true);
-//         var res = xhr.response;
-//         // request.setRequestHeader("Content-Type", "object");
-//        console.log(typeof res);
-//        console.log(res.value);
-//     });
-
 // this changes between search results and main page
 var searchPage = function() {
     $('#results').toggleClass("active");
@@ -143,18 +121,19 @@ var searchPage = function() {
 
 // handles search
 var searchEvent = function(keyword) {
-  $('#resulthead').text('Results for: ' + keyword);
+  var bool = $('#results').hasClass("active");
+  if (bool) $('#resultlist').empty();
+  var searched = {};
   for (s in studies) {
     if (searchMatch(keyword, studies[s])) {
-      addStudy('#resultlist', studies[s]);
+      addStudy('resultlist', studies[s], searched);
     }
   }
-  searchPage();
+  if (!bool) searchPage();
 }
 
 // Helper 4 - string matching
 var searchMatch = function (keyword, study) {
-  keyword = keyword.toLowerCase();
   return (study.ID.indexOf(keyword) >= 0 );
 }
 
@@ -162,7 +141,7 @@ var searchMatch = function (keyword, study) {
 $('#submit').click(function() {
     var task = document.querySelector("input[type=text]")
     var input = task.value.toLowerCase();
-    currItems = {};
+    document.getElementById("searchtop").value = input;    
     searchEvent(input);
 });
 
@@ -170,6 +149,19 @@ $("#searchbar").keyup(function(event){
     if(event.keyCode == 13){
         $("#submit").click();
     }
+});
+
+// searching from the results page
+$('#newsearch').click(function() {
+  var task = document.querySelector("#searchtop");
+  var input = task.value.toLowerCase();
+  searchEvent(input);
+});
+
+$("#searchtop").keyup(function(event){
+  if(event.keyCode == 13){
+      $("#newsearch").click();
+  }
 });
 
 // Changes the page displayed
